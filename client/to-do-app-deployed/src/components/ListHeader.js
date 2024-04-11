ac9564bd389fe05e67d4cef3193b08ab6c248753
Paarth Arora <paarth.arora21@gmail.com>
2024-04-10 21:38:56 -0400

    Added Add New and Sign out buttons to ListHeader

--- client/to-do-app-deployed/src/components/ListHeader.js
+++ client/to-do-app-deployed/src/components/ListHeader.js
@@ -2,6 +2,10 @@ const ListHeader = ({ listName }) => {
     return (
         <div className="list-header">
             <h1>{listName}</h1>
+            <div className="button-container">
+                <button className="create">ADD NEW</button>
+                <button className="signout">SIGN OUT</button>
+            </div>
         </div>
     )
 }
