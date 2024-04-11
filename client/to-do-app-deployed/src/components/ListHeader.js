43a2e3b92dcb60a3ef1d711b475a5143381fc8ff
Paarth Arora <paarth.arora21@gmail.com>
2024-04-10 21:50:48 -0400

    Added signout button and onClick event

--- client/to-do-app-deployed/src/components/ListHeader.js
+++ client/to-do-app-deployed/src/components/ListHeader.js
@@ -1,10 +1,14 @@
 const ListHeader = ({ listName }) => {
+    const signout = () => {
+        console.log("signout")
+    }
+    
     return (
         <div className="list-header">
             <h1>{listName}</h1>
             <div className="button-container">
                 <button className="create">ADD NEW</button>
-                <button className="signout">SIGN OUT</button>
+                <button className="signout" onClick={signout}>SIGN OUT</button>
             </div>
         </div>
     )
