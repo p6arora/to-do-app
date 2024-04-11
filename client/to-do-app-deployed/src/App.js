49085775cd980a08f15f8bfff5ad989ea72166b0
Paarth Arora <paarth.arora21@gmail.com>
2024-04-11 00:13:27 -0400

    Added API call to /todos/:user_email to query the database to get emails associated. Saved it into tasks object using useState and now are logging it onto console

--- client/to-do-app-deployed/src/App.js
+++ client/to-do-app-deployed/src/App.js
@@ -1,6 +1,32 @@
 import ListHeader from "./components/ListHeader";
+import { useEffect, useState } from 'react'
+
 
 const App = () => {
+  const userEmail = 'aniya@test.com'
+  const [tasks, setTasks] = useState(null)
+
+  const getData = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`)
+      const json = await response.json()
+      setTasks(json)
+    }
+    catch (err) {
+      console.error(err)
+    }
+
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  console.log(tasks)
+
+  // Sort by date
+  const sortedTasks = tasks?.sort((a,b) => new Date(a.date) - new Date(b.date))
+
   return (
     <div className="app">
       <ListHeader listName={"🏝️ Holiday tick list"} />
